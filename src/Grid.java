9452397d22192d980a321bd060a33c5fa9391d4e
Tomas Dahlander <tomas.dahlander@gmail.com>
2020-12-16 09:51:44 +0100

    Made a Grid class - Start for Rectangle writer

---
+++ src/Grid.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Tomas Dahlander <br>
+ * Date: 2020-12-16 <br>
+ * Time: 09:49 <br>
+ * Project: Painter <br>
+ */
+public class Grid {
+    private int row;
+    private int col;
+
+    public Grid(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
